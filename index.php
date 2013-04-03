9de9a2757de005bf1a1b1744dd774564837bf15e
Marcos Garcia <marckfree@gmail.com>
2013-04-03 10:44:22 -0300

    Inserindo marcações para as páginas (slides) - válido W3C

--- index.php
+++ index.php
@@ -10,8 +10,91 @@
     <title>Compra Mauá e Região &#8212; Prepara-se! Nós vamos inovar de novo</title>
   </head>
   <body>
+    <div id="pagina">
+      <div class="slide">
+        <div class="header">
+          <h1>Bem-vindo</h1>
+        </div>
 
+        <div class="conteudo">
+          <div class="texto">
+            <p>Nós queremos apresentar o nosso novo site. Use o scroll para nos acompanhar.</p>
+          </div>
+        </div>
+      </div> <!-- #Slide -->
 
+      <div class="slide">
+        <div class="conteudo">
+          <div class="texto">
+            <p>Você já navegou em um site em que você não precisa mudar de página?</p>
+          </div>
+          <div class="logotipo">
+            <img src="#" alt="" />
+          </div>
+        </div>
+      </div> <!-- #Slide -->
+
+      <div class="slide">
+        <div class="conteudo">
+          <div class="texto">
+            <p>Aliás, você já viu um site de compras coletivas para Mauá e Região?</p>
+          </div>
+        </div>
+      </div> <!-- #Slide -->
+
+      <div class="slide">
+        <div class="conteudo">
+          <div class="texto">
+            <p>Um site fácil de mexer no PC e ótimo para navegar em smartphone e tablets.</p>
+          </div>
+          <div class="destaque">
+            <div class="texto">
+              <p>Isso é possível?</p>
+            </div>
+            <div class="smartphone">
+              <img src="#" alt="" />
+            </div>
+          </div>
+        </div>
+      </div> <!-- #Slide -->
+
+      <div class="slide">
+        <div class="conteudo">
+          <div class="header">
+            <h1>Prepare-se.</h1>
+          </div>
+          <div class="texto">
+            <p>Nós vamos inovar de novo!</p>
+          </div>
+          <div class="logotipo-portal">
+            <img src="#" alt="" />
+          </div>
+        </div>
+      </div> <!-- #Slide -->
+
+      <div class="slide">
+        <div class="conteudo">
+          <div class="header-maior">
+            <h1>Aguarde!</h1>
+          </div>
+          <div class="texto">
+            <p>Seja o primeiro a saber:</p>
+          </div>
+          <div class="formulario">
+            <form>
+              <input type="text" value="Nome" name="nome" /> <br />
+              <input type="text" value="E-mail" name="email" />
+              <input type="button" value="OK" name="botao-ok" />
+            </form>
+          </div>
+        </div>
+      </div> <!-- #Slide -->
+
+      <div class="botao-controle">
+        <img src="#" alt="" />
+      </div>
+
+    </div> <!-- #pagina -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="js/scripts.min.js"></script> 
   </body>
