3f125c74a7fb02b9acfd55802ea6d64a0317c370
Marcos Garcia <marckfree@gmail.com>
2013-04-03 16:06:23 -0300

    Adicionando seta lateral para o controle do scroll e formatação geral para os slides

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="keywords" content="compra, maua e regiao, compra coletiva, ribeirao pires, rio grande da serra, diadema, suzano, lancamento maua e regiao, grupo maua e regiao, revista maua e regiao, jornal maua e regiao, portal maua e regiao, loja virtual maua" />
     <meta name="description" content="Este é o hotsite do Compra Mauá e Região, um lançamento do Grupo Mauá e Região para você." />
     <meta name="author" content="Grupo Mauá e Região" />
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato" />
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300" />
     <link rel="stylesheet" href="css/styles.min.css" />
     <title>Compra Mauá e Região &#8212; Prepara-se! Nós vamos inovar de novo</title>
   </head>
@@ -90,12 +90,12 @@
         </div>
       </div> <!-- #Slide -->
 
-      <div class="botao-controle">
-        <img src="#" alt="" />
+      <div class="botao-controle animacao-controle-scroll">
+        <img src="imagens/seta-scroll.png" alt="" />
       </div>
 
     </div> <!-- #pagina -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="js/scripts.min.js"></script> 
+    <script src="js/scripts.js"></script> 
   </body>
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+  function getWidth() {
+    return $(window).width();
+  }
+
+  function getHeight() {
+    return $(window).height();
+  }
+
+  function ajustarSlide() {
+    var slides, janela;
+    slides = $('.slide');
+    janela = $(window);
+
+    slides.css('height', getHeight());
+
+    janela.on('resize', function () {
+      slides.css('height', getWidth());
+    });
+  }
+
+  function posicionarSetaControleScroll() {
+    var controleScroll, janela;
+    controleScroll = $('.botao-controle');
+    janela = $(window);
+
+    controleScroll.css({
+      'top': getHeight() - 100,
+      'left': getWidth() - 150
+    });
+
+    janela.on('resize', function () {
+      controleScroll.css({
+        'top': getHeight() - 100,
+        'left': getWidth() - 150
+      });
+    });
+  }
+
+  function controlarAnimacaoScroll() {
+    var controleScroll;
+    controleScroll = $('.botao-controle');
+
+    controleScroll.on('mouseover', function () {
+      controleScroll.css({
+        '-webkit-animation-play-state': 'paused',
+        '-moz-animation-play-state': 'paused',
+        '-o-animation-play-state': 'paused',
+        'animation-play-state': 'paused'
+      });
+    });
+
+    controleScroll.on('mouseleave', function () {
+      controleScroll.css({
+        '-webkit-animation-play-state': 'running',
+        '-moz-animation-play-state': 'running',
+        '-o-animation-play-state': 'running',
+        'animation-play-state': 'running'
+      });
+    });
+  }
+
+  ajustarSlide();
+  posicionarSetaControleScroll();
+  controlarAnimacaoScroll();
+
+});
