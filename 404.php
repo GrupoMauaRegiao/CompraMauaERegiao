c34cd64b0fd1e8325d188406a8ac1ce6711198da
Marcos Garcia <marckfree@gmail.com>
2013-04-08 13:30:03 -0300

    Adicionando arquivos para controle de erros e redirecionamento

---
+++ 404.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="compra, maua e regiao, compra coletiva, ribeirao pires, rio grande da serra, diadema, suzano, lancamento maua e regiao, grupo maua e regiao, revista maua e regiao, jornal maua e regiao, portal maua e regiao, loja virtual maua" />
+    <meta name="description" content="Este é o hotsite do Compra Mauá e Região, um lançamento do Grupo Mauá e Região para você." />
+    <meta name="author" content="Grupo Mauá e Região" />
+    <link rel="shortcut icon" href="imagens/favicon.ico" />
+    <title>Compra Mauá e Região &#8212; Prepara-se! Nós vamos inovar de novo</title>
+  </head>
+  <body>
+    Esta página não existe. <br />
+    Acesse ===> <a href="http://compramaua.com.br">Compra Mauá e Região</a>
+  </body>
+</html>
