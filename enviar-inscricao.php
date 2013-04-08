fb7c013cc50d15d1a736bac1d392c847e4dbd0c1
Marcos Garcia <marckfree@gmail.com>
2013-04-08 15:57:24 -0300

    Criando exceção para o envio de inscrições

--- enviar-inscricao.php
+++ enviar-inscricao.php
@@ -1,27 +1,4 @@
 <?php
-  if (PATH_SEPARATOR == ";") {
-    $quebraLinha = "\r\n";
-  } else {
-    $quebraLinha = "\n";
-  }
-
-  $destinatario = "news@compramaua.com.br";
-  $assunto = "COMPRA MAUÁ E REGIÃO - Inscrição";
-  $nome = ucwords($_POST["nome"]);
-  $email = strtolower($_POST["email"]);
-  $conteudo = 
-    '<p><b>Nome:</b> ' . $nome . '</p>
-     <p><b>E-mail:</b> ' . $email . '</p>
-     <p><b>Assunto:</b> ' . $assunto . '</p>';
-     
-  $headers .= "MIME-Version: 1.1" . $quebraLinha;
-  $headers .= "Content-type: text/html; charset=utf-8" . $quebraLinha;
-  $headers .= "From: " . $email . $quebraLinha;
-
-  if (!mail($destinatario, $assunto, $conteudo, $headers , "-r" . $destinatario)) {
-    mail($destinatario, $assunto, $conteudo, $headers);
-  }
-
   function criarListaEmails($enderecoEmail, $arquivo) {
     if ($enderecoEmail) {
       if (file_exists($arquivo)) {
@@ -29,6 +6,29 @@
         $conteudo = fread($hdlr, filesize($arquivo));
 
         if (!preg_match("/$enderecoEmail/i", $conteudo)) {
+          
+          if (PATH_SEPARATOR == ";") {
+            $quebraLinha = "\r\n";
+          } else {
+            $quebraLinha = "\n";
+          }
+
+          $destinatario = "news@compramaua.com.br";
+          $assunto = "COMPRA MAUÁ E REGIÃO - QUERO RECEBER AS NOVIDADES";
+          $nome = ucwords($_POST["nome"]);
+          $conteudo = 
+            '<p><b>Nome:</b> ' . $nome . '</p>
+             <p><b>E-mail:</b> ' . $enderecoEmail . '</p>
+             <p><b>Assunto:</b> ' . $assunto . '</p>';
+             
+          $headers .= "MIME-Version: 1.1" . $quebraLinha;
+          $headers .= "Content-type: text/html; charset=utf-8" . $quebraLinha;
+          $headers .= "From: " . $enderecoEmail . $quebraLinha;
+          
+          if (!mail($destinatario, $assunto, $conteudo, $headers , "-r" . $destinatario)) {
+            mail($destinatario, $assunto, $conteudo, $headers);
+          }
+
           fwrite($hdlr, $enderecoEmail . "; ");
           fclose($hdlr);
         }
@@ -36,6 +36,7 @@
     }
   }
 
+  $email = strtolower($_POST["email"]);
   criarListaEmails($email, '/home/comprama/public_html/lista-de-inscricoes.txt');
 
 ?>
